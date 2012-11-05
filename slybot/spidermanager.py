a583f83474b083537b53d760b0a2add1584bd14a
Martin Olveyra <molveyra@gmail.com>
2012-11-05 21:03:20 -0200

    allow spidermanager to instantiate a custom spider class

--- slybot/spidermanager.py
+++ slybot/spidermanager.py
@@ -3,6 +3,7 @@ from zipfile import ZipFile
 
 from zope.interface import implements
 from scrapy.interfaces import ISpiderManager
+from scrapy.utils.misc import load_object
 
 from slybot.spider import IblSpider
 
@@ -10,13 +11,15 @@ class SlybotSpiderManager(object):
 
     implements(ISpiderManager)
 
-    def __init__(self, datadir):
+    def __init__(self, datadir, spider_cls=None):
         self.datadir = datadir
+        self.spider_cls = load_object(spider_cls) if spider_cls else IblSpider
 
     @classmethod
     def from_crawler(cls, crawler):
         datadir = crawler.settings['PROJECT_DIR']
-        return cls(datadir)
+        spider_cls = crawler.settings['SLYBOT_SPIDER_CLASS']
+        return cls(datadir, spider_cls)
 
     def create(self, name, **args):
         with open(os.path.join(self.datadir, 'spiders', '%s.json' % name)) as f:
@@ -24,7 +27,7 @@ class SlybotSpiderManager(object):
         with open(os.path.join(self.datadir, 'extractors.json')) as f:
             extractors = json.load(f)
         items = self._load_items()
-        return IblSpider(name, spec, items, extractors, **args)
+        return self.spider_cls(name, spec, items, extractors, **args)
 
     def list(self):
         return [os.path.splitext(fname)[0] for fname in \
@@ -42,15 +45,15 @@ class SlybotSpiderManager(object):
 
 class ZipfileSlybotSpiderManager(SlybotSpiderManager):
 
-    def __init__(self, datadir, zipfile=None):
+    def __init__(self, datadir, zipfile=None, spider_cls=None):
         if zipfile:
             datadir = tempfile.mkdtemp(prefix='slybot-')
             ZipFile(zipfile).extractall(datadir)
             atexit.register(shutil.rmtree, datadir)
-        super(ZipfileSlybotSpiderManager, self).__init__(datadir)
+        super(ZipfileSlybotSpiderManager, self).__init__(datadir, spider_cls)
 
     @classmethod
     def from_crawler(cls, crawler):
         s = crawler.settings
-        sm = cls(s['PROJECT_DIR'], s['PROJECT_ZIPFILE'])
+        sm = cls(s['PROJECT_DIR'], s['PROJECT_ZIPFILE'], s['SLYBOT_SPIDER_CLASS'])
         return sm
