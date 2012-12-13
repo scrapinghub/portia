dc8e47b20096635288139dc6cf02251618b4f626
Martin Olveyra <molveyra@gmail.com>
2012-12-13 13:11:27 -0200

    added project specs schema validation, using jsonschema library validator and a custom format validation

--- slybot/utils.py
+++ slybot/utils.py
@@ -1,5 +1,6 @@
 from urlparse import urlparse
-
+import os
+import json
 
 def iter_unique_scheme_hostname(urls):
     """Return an iterator of tuples (scheme, hostname) over the given urls,
@@ -11,4 +12,17 @@ def iter_unique_scheme_hostname(urls):
         scheme_hostname.add((p.scheme, p.hostname))
     return list(scheme_hostname)
 
-
+def open_project_from_dir(project_dir):
+    specs = {"spiders": {}}
+    with open(os.path.join(project_dir, "project.json")) as f:
+        specs["project"] = json.load(f)
+    with open(os.path.join(project_dir, "items.json")) as f:
+        specs["items"] = json.load(f)
+    with open(os.path.join(project_dir, "extractors.json")) as f:
+        specs["extractors"] = json.load(f)
+    for fname in os.listdir(os.path.join(project_dir, "spiders")):
+        if fname.endswith(".json"):
+            spider_name = os.path.splitext(fname)[0]
+            with open(os.path.join(project_dir, "spiders", fname)) as f:
+                specs["spiders"][spider_name] = json.load(f)
+    return specs
