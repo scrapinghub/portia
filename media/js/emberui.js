48de0db6436c88e6040b64e9e07e49a0a39d06d4
duendex <duendex@gmail.com>
2013-12-23 13:11:54 -0200

    view.js refactoring & cleanup.

--- media/js/emberui.js
+++ media/js/emberui.js
@@ -1,128 +1,126 @@
 $.widget( "custom.combobox", {
-      _create: function() {
+    _create: function() {
         this.wrapper = $( "<span>" )
-          .addClass( "custom-combobox" )
-          .insertAfter( this.element );
+            .addClass( "custom-combobox" )
+            .insertAfter( this.element );
  
         this.element.hide();
         this._createAutocomplete();
         this._createShowAllButton();
-      },
+    },
  
-      _createAutocomplete: function() {
+    _createAutocomplete: function() {
         var selected = this.element.children( ":selected" ),
-          value = selected.val() ? selected.text() : "";
+            value = selected.val() ? selected.text() : "";
  
         this.input = $( "<input>" )
-          .appendTo( this.wrapper )
-          .val( value )
-          .attr( "title", "" )
-          .addClass( "custom-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left" )
-          .autocomplete({
-            delay: 0,
-            minLength: 0,
-            source: $.proxy( this, "_source" )
-          })
-          .tooltip({
-            tooltipClass: "ui-state-highlight"
-          });
+            .appendTo( this.wrapper )
+            .val( value )
+            .attr( "title", "" )
+            .addClass( "custom-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left" )
+            .autocomplete({
+                delay: 0,
+                minLength: 0,
+                source: $.proxy( this, "_source" )
+            })
+            .tooltip({
+                tooltipClass: "ui-state-highlight"
+            });
  
         this._on( this.input, {
-          autocompleteselect: function( event, ui ) {
-            ui.item.option.selected = true;
-            this._trigger( "select", event, {
-              item: ui.item.option
+            autocompleteselect: function( event, ui ) {
+                ui.item.option.selected = true;
+                this._trigger( "select", event, {
+                item: ui.item.option
             });
-          },
+        },
  
-          autocompletechange: "_removeIfInvalid"
+        autocompletechange: "_removeIfInvalid"
         });
-      },
+    },
  
-      _createShowAllButton: function() {
+    _createShowAllButton: function() {
         var input = this.input,
-          wasOpen = false;
+            wasOpen = false;
  
         $( "<a>" )
-          .attr( "tabIndex", -1 )
-          .appendTo( this.wrapper )
-          .button({
-            icons: {
-              primary: "ui-icon-triangle-1-s"
-            },
-            text: false
-          })
-          .removeClass( "ui-corner-all" )
-          .addClass( "custom-combobox-toggle ui-corner-right" )
-          .mousedown(function() {
-            wasOpen = input.autocomplete( "widget" ).is( ":visible" );
-          })
-          .click(function() {
-            //input.focus();
- 
-            // Close if already visible
-            if ( wasOpen ) {
-              return;
+            .attr( "tabIndex", -1 )
+            .appendTo( this.wrapper )
+            .button({
+                icons: {
+                    primary: "ui-icon-triangle-1-s"
+                },
+                text: false
+            })
+            .removeClass( "ui-corner-all" )
+            .addClass( "custom-combobox-toggle ui-corner-right" )
+            .mousedown(function() {
+                wasOpen = input.autocomplete( "widget" ).is( ":visible" );
+            })
+            .click(function() {
+                // Close if already visible
+                if ( wasOpen ) {
+                    return;
             }
  
             // Pass empty string as value to search for, displaying all results
             input.autocomplete( "search", "" );
-          });
-      },
+        });
+    },
  
-      _source: function( request, response ) {
+    _source: function( request, response ) {
         var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
         response( this.element.children( "option" ).map(function() {
-          var text = $( this ).text();
-          if ( this.value && ( !request.term || matcher.test(text) ) )
-            return {
-              label: text,
-              value: text,
-              option: this
+            var text = $( this ).text();
+            if ( this.value && ( !request.term || matcher.test(text) ) )
+                return {
+                    label: text,
+                    value: text,
+                    option: this
             };
-        }) );
-      },
+        }));
+    },
  
-      _removeIfInvalid: function( event, ui ) {
+    _removeIfInvalid: function( event, ui ) {
  
-        // Selected an item, nothing to do
+       // Selected an item, nothing to do
         if ( ui.item ) {
-          return;
+            return;
         }
  
         // Search for a match (case-insensitive)
         var value = this.input.val(),
-          valueLowerCase = value.toLowerCase(),
-          valid = false;
+            valueLowerCase = value.toLowerCase(),
+            valid = false;
         this.element.children( "option" ).each(function() {
-          if ( $( this ).text().toLowerCase() === valueLowerCase ) {
-            this.selected = valid = true;
-            return false;
-          }
+            if ( $( this ).text().toLowerCase() === valueLowerCase ) {
+                this.selected = valid = true;
+                return false;
+            }
         });
  
         // Found a match, nothing to do
         if ( valid ) {
-          return;
+            return;
         }
  
         // Remove invalid value
         this.input
-          .val( "" )
-          .attr( "title", value + " didn't match any item" )
-          .tooltip( "open" );
+            .val( "" )
+            .attr( "title", value + " didn't match any item" )
+            .tooltip( "open" );
         this.element.val( "" );
         this._delay(function() {
-          this.input.tooltip( "close" ).attr( "title", "" );
+            this.input.tooltip( "close" ).attr( "title", "" );
         }, 2500 );
         this.input.data( "ui-autocomplete" ).term = "";
-      },
+    },
  
-      _destroy: function() {
+    _destroy: function() {
         this.wrapper.remove();
         this.element.show();
-      }
-    });
+    }
+});
 
 
 
@@ -134,95 +132,94 @@ JQ = Ember.Namespace.create();
 // Create a new mixin for jQuery UI widgets using the Ember
 // mixin syntax.
 JQ.Widget = Em.Mixin.create({
-  // When Ember creates the view's DOM element, it will call this
-  // method.
-  didInsertElement: function() {
-    // Make jQuery UI options available as Ember properties
-    var options = this._gatherOptions();
+    // When Ember creates the view's DOM element, it will call this
+    // method.
+    didInsertElement: function() {
+        // Make jQuery UI options available as Ember properties
+        var options = this._gatherOptions();
 
-    // Make sure that jQuery UI events trigger methods on this view.
-    this._gatherEvents(options);
+        // Make sure that jQuery UI events trigger methods on this view.
+        this._gatherEvents(options);
 
-    // Create a new instance of the jQuery UI widget based on its `uiType`
-    // and the current element.
-    var ui;
-    if (jQuery.ui[this.get('uiType')]) {
-      ui = jQuery.ui[this.get('uiType')](options, this.get('element'));
-    } else {
-      ui = jQuery.custom[this.get('uiType')](options, this.get('element'));
-    }
-
-    // Save off the instance of the jQuery UI widget as the `ui` property
-    // on this Ember view.
-    this.set('ui', ui);
-  },
-
-  // When Ember tears down the view's DOM element, it will call
-  // this method.
-  willDestroyElement: function() {
-    var ui = this.get('ui');
-
-    if (ui) {
-      // Tear down any observers that were created to make jQuery UI
-      // options available as Ember properties.
-      var observers = this._observers;
-      for (var prop in observers) {
-        if (observers.hasOwnProperty(prop)) {
-          this.removeObserver(prop, observers[prop]);
+        // Create a new instance of the jQuery UI widget based on its `uiType`
+        // and the current element.
+        var ui;
+        if (jQuery.ui[this.get('uiType')]) {
+            ui = jQuery.ui[this.get('uiType')](options, this.get('element'));
+        } else {
+            ui = jQuery.custom[this.get('uiType')](options, this.get('element'));
         }
-      }
-      ui._destroy();
+
+        // Save off the instance of the jQuery UI widget as the `ui` property
+        // on this Ember view.
+        this.set('ui', ui);
+    },
+
+    // When Ember tears down the view's DOM element, it will call
+    // this method.
+    willDestroyElement: function() {
+        var ui = this.get('ui');
+
+        if (ui) {
+            // Tear down any observers that were created to make jQuery UI
+            // options available as Ember properties.
+            var observers = this._observers;
+            for (var prop in observers) {
+                if (observers.hasOwnProperty(prop)) {
+                    this.removeObserver(prop, observers[prop]);
+                }
+            }
+            ui._destroy();
+        }
+    },
+
+    // Each jQuery UI widget has a series of options that can be configured.
+    // For instance, to disable a button, you call
+    // `button.options('disabled', true)` in jQuery UI. To make this compatible
+    // with Ember bindings, any time the Ember property for a
+    // given jQuery UI option changes, we update the jQuery UI widget.
+    _gatherOptions: function() {
+        var uiOptions = this.get('uiOptions'), options = {};
+        // The view can specify a list of jQuery UI options that should be treated
+        // as Ember properties.
+        uiOptions.forEach(function(key) {
+            options[key] = this.get(key);
+
+            // Set up an observer on the Ember property. When it changes,
+            // call jQuery UI's `option` method to reflect the property onto
+            // the jQuery UI widget.
+            var observer = function() {
+                var value = this.get(key);
+                this.get('ui').option(key, value);
+            };
+
+            this.addObserver(key, observer);
+
+            // Insert the observer in a Hash so we can remove it later.
+            this._observers = this._observers || {};
+            this._observers[key] = observer;
+        }, this);
+        return options;
+    },
+
+    // Each jQuery UI widget has a number of custom events that they can
+    // trigger. For instance, the progressbar widget triggers a `complete`
+    // event when the progress bar finishes. Make these events behave like
+    // normal Ember events. For instance, a subclass of JQ.ProgressBarView
+    // could implement the `complete` method to be notified when the jQuery
+    // UI widget triggered the event.
+    _gatherEvents: function(options) {
+        var uiEvents = this.get('uiEvents') || [], self = this;
+
+        uiEvents.forEach(function(event) {
+            var callback = self[event];
+
+            if (callback) {
+                // You can register a handler for a jQuery UI event by passing
+                // it in along with the creation options. Update the options hash
+                // to include any event callbacks.
+                 options[event] = function(event, ui) { callback.call(self, event, ui); };
+            }
+        });
     }
-  },
-
-  // Each jQuery UI widget has a series of options that can be configured.
-  // For instance, to disable a button, you call
-  // `button.options('disabled', true)` in jQuery UI. To make this compatible
-  // with Ember bindings, any time the Ember property for a
-  // given jQuery UI option changes, we update the jQuery UI widget.
-  _gatherOptions: function() {
-    var uiOptions = this.get('uiOptions'), options = {};
-    // The view can specify a list of jQuery UI options that should be treated
-    // as Ember properties.
-    uiOptions.forEach(function(key) {
-      options[key] = this.get(key);
-
-      // Set up an observer on the Ember property. When it changes,
-      // call jQuery UI's `option` method to reflect the property onto
-      // the jQuery UI widget.
-      var observer = function() {
-        var value = this.get(key);
-        this.get('ui').option(key, value);
-      };
-
-      this.addObserver(key, observer);
-
-      // Insert the observer in a Hash so we can remove it later.
-      this._observers = this._observers || {};
-      this._observers[key] = observer;
-    }, this);
-
-    return options;
-  },
-
-  // Each jQuery UI widget has a number of custom events that they can
-  // trigger. For instance, the progressbar widget triggers a `complete`
-  // event when the progress bar finishes. Make these events behave like
-  // normal Ember events. For instance, a subclass of JQ.ProgressBarView
-  // could implement the `complete` method to be notified when the jQuery
-  // UI widget triggered the event.
-  _gatherEvents: function(options) {
-    var uiEvents = this.get('uiEvents') || [], self = this;
-
-    uiEvents.forEach(function(event) {
-      var callback = self[event];
-
-      if (callback) {
-        // You can register a handler for a jQuery UI event by passing
-        // it in along with the creation options. Update the options hash
-        // to include any event callbacks.
-        options[event] = function(event, ui) { callback.call(self, event, ui); };
-      }
-    });
-  }
 });
--- media/js/views.js
+++ media/js/views.js
@@ -1,68 +1,26 @@
-JQ.FollowSelect = Ember.Select.extend(JQ.Widget, {
+/*************************** Base views *****************************/
+
+ASTool.Select = Ember.Select.extend(JQ.Widget, {
 	uiType: 'combobox',
-
 	uiOptions: ['label', 'disabled'],
-
 	uiEvents: ['select'],
-
-	content: [{ option: 'none', label: "Don't follow links" },
-			  { option: 'patterns', label: 'Follow links that match the following patterns' }],
-
 	optionValuePath: 'content.option',
-
 	optionLabelPath: 'content.label',
-
-	select: function(event, data) {
-		this.set('controller.model.links_to_follow', data.item.value);
-	},
 });
 
-JQ.VariantSelect = Ember.Select.extend(JQ.Widget, {
-	uiType: 'combobox',
 
-	uiOptions: ['label', 'disabled'],
-
-	uiEvents: ['select'],
-
-	content: function() {
-		var options = [{ option: '0', label: "Base(0)" }];
-		var maxVariant = this.get('controller.controllers.annotations.maxVariant');
-		var i;
-		for (i = 1; i <= maxVariant; i++) {
-			options.pushObject({ option: i + '', label: 'Variant ' + i });
-		}
-		options.pushObject({ option: i + '', label: 'Add new variant (' + i + ')' });
-		return options;
-	}.property('controller.controllers.annotations.maxVariant'),
-
-	optionValuePath: 'content.option',
-
-	optionLabelPath: 'content.label',
-
-	select: function(event, data) {
-		this.set('controller.model.variant', data.item.value);
-	},
-});
-
-// Create a new Ember view for the jQuery UI Button widget
-JQ.ButtonView = Em.View.extend(JQ.Widget, {
+ASTool.ButtonView = Em.View.extend(JQ.Widget, {
 	uiType: 'button',
-  
 	uiOptions: ['label', 'disabled', 'icons', 'text', 'selected'],
-  
 	tagName: 'button',
-
 	classNames: ['controlShadow'],
-
 	minWidth: null,
-
 	maxWidth: null,
-
 	action: null,
-
 	argument: null,
-
 	attributeBindings: ['name', 'title'],
+	_label: null,
+	title: null,
 
 	name: function() {
 		return this.get('action') + '_' + this.get('label');
@@ -75,10 +33,6 @@ JQ.ButtonView = Em.View.extend(JQ.Widget, {
 	click: function() {
   		this.get('controller').send(this.get('action'), this.get('argument'));
 	},
-
-	_label: null,
-
-	title: null,
 
 	label: function(key, label) {
 		if (arguments.length > 1) {
@@ -101,89 +55,11 @@ JQ.ButtonView = Em.View.extend(JQ.Widget, {
 	},
 });
 
-JQ.AnnotationWidget = JQ.ButtonView.extend({
 
-	mouseEnter: function() {
-		this.set('argument.highlighted', true);
-		this.get('controller').send('annotationHighlighted', this.argument);
-	},
-
-	mouseLeave: function() {
-		this.set('argument.highlighted', false);
-	},	
-});
-
-JQ.CSSPathWidget = JQ.ButtonView.extend({
-
-	mouseEnter: function() {
-		this.get('controller').send('highlightElement', this.get('argument'));
-	},
-
-	mouseLeave: function() {
-		this.get('controller').send('highlightElement', null);
-	},
-});
-
-JQ.ButtonSetView = Em.View.extend(JQ.Widget, {
-	uiType: 'buttonset',
-	uiOptions: [],
-	tagName: 'div',
-});
-
-JQ.TabNavigator = Em.View.extend({
-});
-
-// Create a new Ember view for the jQuery UI Menu widget.
-// Because it wraps a collection, we extend from
-// Ember's CollectionView rather than a normal view.
-//
-// This means that you should use `#collection` in your template to
-// create this view.
-JQ.MenuView = Em.CollectionView.extend(JQ.Widget, {
-  uiType: 'menu',
-  uiOptions: ['disabled'],
-  uiEvents: ['select'],
-
-  tagName: 'ul',
-
-  // Whenever the underlying Array for this `CollectionView` changes,
-  // refresh the jQuery UI widget.
-  arrayDidChange: function(content, start, removed, added) {
-    this._super(content, start, removed, added);
-
-    var ui = this.get('ui');
-    if (ui) {
-      // Schedule the refresh for after Ember has completed it's
-      // render cycle
-      Em.run.scheduleOnce('afterRender', ui, ui.refresh);
-    }
-  },
-  itemViewClass: Em.View.extend({
-    // Make it so that the default context for evaluating handlebars
-    // bindings is the content of this child view.
-    context: function(){
-      return this.get('content');
-    }.property('content')
-  })
-});
-
-// Create a new Ember view for the jQuery UI Progress Bar widget
-JQ.ProgressBarView = Em.View.extend(JQ.Widget, {
-  uiType: 'progressbar',
-  uiOptions: ['value', 'max'],
-  uiEvents: ['change', 'complete']
-});
-
-JQ.TextField = Em.TextField.extend({
-	uiOptions: [],
-	
-	uiType: null,
-
+ASTool.TextField = Em.TextField.extend({
 	width:null,
-
-	attributeBindings: ['placeholder'],
-
 	placeholder: null,
+	attributeBindings: ['placeholder'],
 	
 	didInsertElement: function() {
 		this._super();
@@ -196,7 +72,66 @@ JQ.TextField = Em.TextField.extend({
 	},
 });
 
-JQ.IgnoreWidget = JQ.TextField.extend({
+
+/************************* Application views ****************************/
+
+ASTool.FollowSelect = ASTool.Select.extend({
+	content: [{ option: 'none', label: "Don't follow links" },
+			  { option: 'patterns', label: 'Follow links that match the following patterns' }],
+
+	select: function(event, data) {
+		// FIXME: raise an event instead of directly setting the property.
+		this.set('controller.model.links_to_follow', data.item.value);
+	},
+});
+
+
+ASTool.VariantSelect = ASTool.Select.extend({
+
+	content: function() {
+		var options = [{ option: '0', label: "Base(0)" }];
+		var maxVariant = this.get('controller.controllers.annotations.maxVariant');
+		var i;
+		for (i = 1; i <= maxVariant; i++) {
+			options.pushObject({ option: i + '', label: 'Variant ' + i });
+		}
+		options.pushObject({ option: i + '', label: 'Add new variant (' + i + ')' });
+		return options;
+	}.property('controller.controllers.annotations.maxVariant'),
+
+	select: function(event, data) {
+		// FIXME: raise an event instead of directly setting the property.
+		this.set('controller.model.variant', data.item.value);
+	},
+});
+
+
+ASTool.AnnotationWidget = ASTool.ButtonView.extend({
+
+	mouseEnter: function() {
+		this.set('argument.highlighted', true);
+		this.get('controller').send('annotationHighlighted', this.argument);
+	},
+
+	mouseLeave: function() {
+		this.set('argument.highlighted', false);
+	},	
+});
+
+
+ASTool.CSSPathWidget = ASTool.ButtonView.extend({
+
+	mouseEnter: function() {
+		this.get('controller').send('highlightElement', this.get('argument'));
+	},
+
+	mouseLeave: function() {
+		this.get('controller').send('highlightElement', null);
+	},
+});
+
+
+ASTool.IgnoreWidget = ASTool.TextField.extend({
 	ignore: null,
 	valueBinding: 'ignore.name',
 	
@@ -212,17 +147,7 @@ JQ.IgnoreWidget = JQ.TextField.extend({
 });
 
 
-/*************************** Components **************************/
-
-ASTool.MyButtonComponent = Ember.Component.extend({
-	click: function() {
-		this.sendAction();
-	}
-});
-
-/*************************** Views ********************************/
-
-ASTool.ElemAttributeView = JQ.ButtonView.extend({
+ASTool.ElemAttributeView = ASTool.ButtonView.extend({
 	value: null,
 	attribute: null,
 	
@@ -232,7 +157,7 @@ ASTool.ElemAttributeView = JQ.ButtonView.extend({
 		var ui = $(this.get('element'));
 		var content = $('<div/>').
 			append($('<span/>', { text:attribute.name + ': ', class:'elementAttributeName' })).
-			append($('<span/>', { text:trim(attribute.value, 40), class:'elementAttributeValue' }));
+			append($('<span/>', { text:trim(attribute.value, 34), class:'elementAttributeValue' }));
 		if (this.mapped) {
 			content.append($('<div/>').
 			append($('<span/>', { text:'mapped to: ', class:'elementAttributeName' })).
@@ -244,23 +169,16 @@ ASTool.ElemAttributeView = JQ.ButtonView.extend({
 	},
 });
 
+
 ASTool.ItemView = Ember.View.extend({
 	templateName: 'item',
 	item: null,
 	mappingAttribute: null,
 });
 
-JQ.EditField = JQ.TextField.extend({
-	change: function(evt) {
-		//this.get('owner').save();
-	}
-});
 
-ASTool.EditBox = Ember.Checkbox.extend({
-	owner: null,
-	change: function(evt) {
-	}
-});
+ASTool.EditBox = Ember.Checkbox.extend({});
+
 
 ASTool.TypeSelect = Ember.Select.extend({
 	owner: null,
@@ -269,6 +187,7 @@ ASTool.TypeSelect = Ember.Select.extend({
 		this.get('owner').save();
 	}
 });
+
 
 ASTool.AnnotatedDocumentView = Ember.View.extend({
 	templateName: 'annotated-document-view',
@@ -282,7 +201,8 @@ ASTool.AnnotatedDocumentView = Ember.View.extend({
 	},
 });
 
-ASTool.ViewNotifyingMixin = Ember.Mixin.create({
+
+ASTool.ToolbarViewMixin = Ember.Mixin.create({
 	willInsertElement: function() {
 		if (this.get('controller.willEnter')) {
 			this.controller.willEnter();	
@@ -302,11 +222,11 @@ ASTool.ViewNotifyingMixin = Ember.Mixin.create({
 });
 
 
-ASTool.AnnotationsView = Ember.View.extend(ASTool.ViewNotifyingMixin);
-ASTool.AnnotationView = Ember.View.extend(ASTool.ViewNotifyingMixin);
-ASTool.ItemsView = Ember.View.extend(ASTool.ViewNotifyingMixin);
-ASTool.SpiderView = Ember.View.extend(ASTool.ViewNotifyingMixin);
-ASTool.ProjectView = Ember.View.extend(ASTool.ViewNotifyingMixin);
+ASTool.AnnotationsView = Ember.View.extend(ASTool.ToolbarViewMixin);
+ASTool.AnnotationView = Ember.View.extend(ASTool.ToolbarViewMixin);
+ASTool.ItemsView = Ember.View.extend(ASTool.ToolbarViewMixin);
+ASTool.SpiderView = Ember.View.extend(ASTool.ToolbarViewMixin);
+ASTool.ProjectView = Ember.View.extend(ASTool.ToolbarViewMixin);
 
 
 /*************************** Helpers ******************************/
