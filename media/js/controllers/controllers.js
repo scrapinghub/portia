76de3c85b97e832451a7ce1dcc558a1bb862fec2
duendex <duendex@gmail.com>
2014-01-30 10:42:17 -0200

    Moved route browser code.

--- media/js/controllers/controllers.js
+++ media/js/controllers/controllers.js
@@ -1,19 +1,64 @@
 ASTool.RouteBrowseMixin = Ember.Mixin.create({
+	modelMap: {},
+
+	labelMap: {},
+
+	routeStack: [],
+	
+	documentView: null,
 
 	pushRoute: function(route, label, animation, model) {
-		this.get('controllers.application').pushRoute(route, label, animation, model);
+		if (this.routeStack.filterBy('route', route).length) {
+			// Don't go if the route is already in the stack.
+			return;
+		}
+		animation = animation || 'fade';
+		var navRoute = ASTool.NavRoute.create({ route: route,
+												label: label,
+												model: model });
+		this.routeStack.pushObject(navRoute);
+		if (model) {
+			this.transitionToRouteAnimated(route, {main: animation}, model);		
+		} else {
+			this.transitionToRouteAnimated(route, {main: animation});
+		}
 	},
 
 	popRoutes: function(route, animation) {
-		this.get('controllers.application').popRoutes(route, animation);
+		animation = animation || 'fade';
+		var navRoute = this.routeStack.filterBy('route', route).get('firstObject');
+		if (navRoute) {
+			var tmp = this.routeStack.toArray();
+			var found = false;
+			tmp.forEach(function(navRoute) {
+				if (found) {
+					this.routeStack.removeObject(navRoute);
+				}
+				if (navRoute.route == route) {
+					found = true;
+				}
+			}.bind(this));
+			var lastRoute = this.routeStack.get('lastObject');
+			if (lastRoute.model) {
+				this.transitionToRouteAnimated(lastRoute.route, {main: animation}, lastRoute.model);	
+			} else {
+				this.transitionToRouteAnimated(lastRoute.route, {main: animation});
+			}
+		}
 	},
 
 	popRoute: function(animation) {
-		this.get('controllers.application').popRoute(animation);
+		if (this.routeStack.length > 1) {
+			this.popRoutes(this.routeStack[this.routeStack.length - 2].route, animation);
+		}
 	},
 
 	updateTop: function(newLabel, newModel) {
-		this.get('controllers.application').updateTop(newLabel, newModel);	
+		var topRoute = this.routeStack.get('lastObject');
+		topRoute.set('label', newLabel);
+		if (newModel) {
+			topRoute.set('model', newModel);
+		}
 	},
 
 	transitionToRouteAnimated: function(route, animation, model) {
@@ -38,7 +83,9 @@ ASTool.RouteBrowseMixin = Ember.Mixin.create({
 		back: function(animation) {
 			this.popRoute(animation);	
 		},
+
+		gotoRoute: function(route, animation) {
+			this.popRoutes(route, animation);
+		},
 	},
 });
-
-
