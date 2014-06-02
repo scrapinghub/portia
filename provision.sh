f5bc4c480ecaf2b5a85dd4733093deaa05ee88be
duendex <duendex@gmail.com>
2014-06-02 13:17:50 -0300

    sudo is not needed in the provisioning script.

--- provision.sh
+++ provision.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 echo "Updating apt-get"
 echo "================"
-sudo apt-get update
+apt-get update
 echo "Installing python stuff"
 echo "======================="
-sudo apt-get install -y --force-yes python-dev libxml2-dev libxslt1-dev libffi-dev libssl-dev
+apt-get install -y --force-yes python-dev libxml2-dev libxslt1-dev libffi-dev libssl-dev
 echo "Installing python pip"
 echo "====================="
-sudo apt-get install -y --force-yes python-pip
+apt-get install -y --force-yes python-pip
 echo "Installing git"
 echo "=============="
-sudo apt-get install -y --force-yes git
+apt-get install -y --force-yes git
 echo "Installing portia dependencies"
 echo "=============================="
 cd /vagrant/slyd
 pip install -r requirements.txt
 echo "Installing slyd as a Upstart service"
 echo "===================================="
-sudo cp /vagrant/slyd.conf /etc/init
+cp /vagrant/slyd.conf /etc/init
 echo "Starting slyd service"
 echo "====================="
-sudo start slyd
+start slyd
 
 
 
