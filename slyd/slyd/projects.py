93d6bba211d4a49336d8b5f9c8aa500a0d3aac94
duendex <duendex@gmail.com>
2014-06-04 14:54:54 -0300

    Adds GIT repo support to slyd project API

--- slyd/slyd/projects.py
+++ slyd/slyd/projects.py
@@ -50,22 +50,29 @@ class ProjectsResource(SlydJsonResource):
             if ex.errno != errno.ENOENT:
                 raise
 
-    def create_project(self, project_name):
-        project_filename = self.project_filename(project_name)
+    def create_project(self, name):
+        self.validate_project_name(name)
+        project_filename = self.project_filename(name)
         os.makedirs(project_filename)
         with open(join(project_filename, 'project.json'), 'wb') as outf:
             outf.write('{}')
         os.makedirs(join(project_filename, 'spiders'))
 
     def rename_project(self, from_name, to_name):
+        self.validate_project_name(from_name)
+        self.validate_project_name(to_name)
         os.rename(self.project_filename(from_name),
             self.project_filename(to_name))
 
     def remove_project(self, name):
         shutil.rmtree(self.project_filename(name))
 
-    def project_filename(self, project_name):
-        return join(self.projectsdir, project_name)
+    def project_filename(self, name):
+        return join(self.projectsdir, name)
+
+    def validate_project_name(self, name):
+        if not allowed_project_name(name):
+            self.bad_request('invalid project name %s' % project)
 
     def handle_project_command(self, command_spec):
         command = command_spec.get('cmd')
@@ -74,10 +81,7 @@ class ProjectsResource(SlydJsonResource):
             self.bad_request(
                 "unrecognised cmd arg %s, available commands: %s" %
                 (command, ', '.join(self.project_commands.keys())))
-        args = command_spec.get('args', [])
-        for project in args:
-            if not allowed_project_name(project):
-                self.bad_request('invalid project name %s' % project)
+        args = command_spec.get('args', [])        
         try:
             retval = dispatch_func(self, *args)
         except TypeError:
@@ -111,41 +115,53 @@ class GitProjectsResource(ProjectsResource):
         SlydJsonResource.__init__(self)
         self.projectsdir = settings['GIT_SPEC_DATA_DIR']
 
-    def create_project(self, project_name):
-        project_filename = self.project_filename(project_name)
-        repoman = Repoman.create_repo(project_filename)
+    def open_repo(self, name):
+        return Repoman.open_repo(self.project_filename(name))
+
+    def create_project(self, name):
+        self.validate_project_name(name)
+        repoman = Repoman.create_repo(self.project_filename(name))
         repoman.save_file('project.json', '{}', 'master')
 
     def remove_project(self, name):
         Repoman.delete_repo(self.project_filename(name))
 
     def edit_project(self, name, revision):
-        project_filename = self.project_filename(name)
-        repoman = Repoman.open_repo(project_filename)
+        repoman = self.open_repo(name)
         if revision == 'master':
             revision = repoman.get_branch('master')
         if not repoman.has_branch(self.user):
             repoman.create_branch(self.user, revision)
 
-    def publish_project(self, name):
-        project_filename = self.project_filename(name)
-        repoman = Repoman.open_repo(project_filename)
-        if repoman.publish_branch(self.user):
+    def publish_project(self, name, force):
+        repoman = self.open_repo(name)
+        if repoman.publish_branch(self.user, force):
             repoman.delete_branch(self.user)
             return 'OK'
         else:
             return 'CONFLICT'
 
     def discard_changes(self, name):
-        project_filename = self.project_filename(name)
-        repoman = Repoman.open_repo(project_filename)
+        repoman = self.open_repo(name)
         repoman.delete_branch(self.user)
 
     def project_revisions(self, name):
-        project_filename = self.project_filename(name)
-        repoman = Repoman.open_repo(project_filename)
-        revisions = repoman.get_published_revisions()
-        return json.dumps({ 'revisions': revisions })
+        repoman = self.open_repo(name)
+        return json.dumps({ 'revisions': repoman.get_published_revisions() })
+
+    def conflicted_files(self, name):        
+        repoman = self.open_repo(name)
+        return json.dumps(repoman.get_branch_conflicted_files(self.user))
+
+    def changed_files(self, name):        
+        repoman = self.open_repo(name)
+        return json.dumps(repoman.get_branch_changed_files(self.user))
+
+    def save_file(self, project_name, file_path, file_contents):
+        repoman = self.open_repo(project_name)
+        repoman.save_file(file_path,
+                          json.dumps(json_contents, sort_keys=True, indent=4),
+                          self.user)
 
     project_commands = {
         'create': create_project,
@@ -155,4 +171,7 @@ class GitProjectsResource(ProjectsResource):
         'publish': publish_project,
         'discard': discard_changes,
         'revisions': project_revisions,
+        'conflicts': conflicted_files,
+        'changes': changed_files,
+        'save': save_file,
     }
