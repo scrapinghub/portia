565861256c9cf0f41217df13c4244315b4ebd74d
Martin Olveyra <molveyra@gmail.com>
2014-07-31 11:02:34 -0200

    Remove deprecation warning by using new style item pipeline definition

--- slybot/slybot/settings.py
+++ slybot/slybot/settings.py
@@ -1,6 +1,6 @@
 SPIDER_MANAGER_CLASS = 'slybot.spidermanager.SlybotSpiderManager'
 EXTENSIONS = {'slybot.closespider.SlybotCloseSpider': 1}
-ITEM_PIPELINES = ['slybot.dupefilter.DupeFilterPipeline']
+ITEM_PIPELINES = {'slybot.dupefilter.DupeFilterPipeline': 1}
 SPIDER_MIDDLEWARES = {'slybot.spiderlets.SpiderletsMiddleware': 999} # as close as possible to spider output
 SLYDUPEFILTER_ENABLED = True
 PROJECT_DIR = 'slybot-project'
