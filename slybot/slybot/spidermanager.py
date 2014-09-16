a6fcb75620dac5f5574f90a7931293e8184b8d11
Mikhail Korobov <kmike84@gmail.com>
2014-09-17 04:07:28 +0600

    from_settings method for Scrapy 0.25+ compatibility

--- slybot/slybot/spidermanager.py
+++ slybot/slybot/spidermanager.py
@@ -8,6 +8,7 @@ from scrapy.utils.misc import load_object
 from slybot.spider import IblSpider
 from slybot.utils import open_project_from_dir
 
+
 class SlybotSpiderManager(object):
 
     implements(ISpiderManager)
@@ -18,11 +19,17 @@ class SlybotSpiderManager(object):
 
     @classmethod
     def from_crawler(cls, crawler):
-        datadir = crawler.settings['PROJECT_DIR']
-        spider_cls = crawler.settings['SLYBOT_SPIDER_CLASS']
+        # backwards compatibility with Scrapy < 0.25
+        return cls.from_settings(crawler.settings)
+
+    @classmethod
+    def from_settings(cls, settings):
+        datadir = settings['PROJECT_DIR']
+        spider_cls = settings['SLYBOT_SPIDER_CLASS']
         return cls(datadir, spider_cls)
 
     def create(self, name, **args):
+        # backwards compatibility with Scrapy < 0.25
         spec = self._specs["spiders"][name]
         items = self._specs["items"]
         extractors = self._specs["extractors"]
@@ -30,6 +37,7 @@ class SlybotSpiderManager(object):
 
     def list(self):
         return self._specs["spiders"].keys()
+
 
 class ZipfileSlybotSpiderManager(SlybotSpiderManager):
 
@@ -41,7 +49,8 @@ class ZipfileSlybotSpiderManager(SlybotSpiderManager):
         super(ZipfileSlybotSpiderManager, self).__init__(datadir, spider_cls)
 
     @classmethod
-    def from_crawler(cls, crawler):
-        s = crawler.settings
-        sm = cls(s['PROJECT_DIR'], s['PROJECT_ZIPFILE'], s['SLYBOT_SPIDER_CLASS'])
-        return sm
+    def from_settings(cls, settings):
+        datadir = settings['PROJECT_DIR']
+        zipfile = settings['PROJECT_ZIPFILE']
+        spider_cls = settings['SLYBOT_SPIDER_CLASS']
+        return cls(datadir, zipfile, spider_cls)
