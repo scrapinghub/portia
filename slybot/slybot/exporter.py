dd2de7498f25ac21522f157cf02ebd3e76fab8ac
tpeng <pengtaoo@gmail.com>
2014-10-08 11:44:50 +0200

    customize export fields in csv exporter

---
+++ slybot/slybot/exporter.py
@@ -0,0 +1,7 @@
+from scrapy.contrib.exporter import CsvItemExporter
+from scrapy.conf import settings
+
+class SlybotCSVItemExporter(CsvItemExporter):
+    def __init__(self, *args, **kwargs):
+        kwargs['fields_to_export'] = settings.getlist('CSV_EXPORT_FIELDS') or None
+        super(SlybotCSVItemExporter, self).__init__(*args, **kwargs)
--- slybot/slybot/settings.py
+++ slybot/slybot/settings.py
@@ -4,6 +4,10 @@ ITEM_PIPELINES = {'slybot.dupefilter.DupeFilterPipeline': 1}
 SPIDER_MIDDLEWARES = {'slybot.spiderlets.SpiderletsMiddleware': 999} # as close as possible to spider output
 SLYDUPEFILTER_ENABLED = True
 PROJECT_DIR = 'slybot-project'
+FEED_EXPORTERS = {
+    'csv': 'slybot.exporter.SlybotCSVItemExporter',
+}
+CSV_EXPORT_FIELDS = None
 
 try:
     from local_slybot_settings import *
