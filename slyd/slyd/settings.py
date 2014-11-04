30918a372ae5f5341f3e4d2213344c8d21969ba2
duendex <duendex@gmail.com>
2014-11-04 11:07:31 -0200

    Cleaned up settings

--- slyd/slyd/settings.py
+++ slyd/slyd/settings.py
@@ -12,78 +12,19 @@ LOG_LEVEL = 'DEBUG'
 
 # location of slybot projects - assumes a subdir per project
 DATA_DIR = join(dirname(dirname(__file__)), 'data')
-SPEC_DATA_DIR = join(DATA_DIR, 'gitprojects')
-MYSQL_DB = 'mysql://portia:portia@frankie:3306/portia'
-#DASH_API_URL = 'http://marcos-portia-integration.demo.scrapinghub.com/api/'
-DASH_API_URL = 'http://33.33.33.10:8000/api/'
+SPEC_DATA_DIR = join(DATA_DIR, 'projects')
 
-#################### FILESYSTEM ################################
-'''
 SPEC_FACTORY = {
-	'PROJECT_SPEC': 'slyd.projectspec.GitProjectSpec',
-	'PROJECT_MANAGER': 'slyd.projectspec.GitProjectsManager',
+	'PROJECT_SPEC': 'slyd.projectspec.ProjectSpec',
+	'PROJECT_MANAGER': 'slyd.projectspec.ProjectsManager',
 	'PARAMS': {
 		'location': SPEC_DATA_DIR,
 	},
 	'CAPABILITIES': {
 		'version_control': False,
 		'create_projects': True,
-	}
-}
-'''
-
-#################### GIT MYSQL ################################
-'''
-SPEC_FACTORY = {
-	'PROJECT_SPEC': 'slyd.gitstorage.projectspec.GitProjectSpec',
-	'PROJECT_MANAGER': 'slyd.gitstorage.projects.GitProjectsManager',
-	'PARAMS': {
-		'storage_backend': 'dulwich.mysqlrepo.MysqlRepo',
-		'location': MYSQL_DB,
-	},
-	'CAPABILITIES': {
-		'version_control': True,
-		'create_projects': True,
-	}
-}
-'''
-
-#################### GIT FS ###################################
-'''
-SPEC_FACTORY = {
-	'PROJECT_SPEC': 'slyd.gitstorage.projectspec.GitProjectSpec',
-	'PROJECT_MANAGER': 'slyd.gitstorage.projects.GitProjectsManager',
-	'PARAMS': {
-		'storage_backend': 'dulwich.fsrepo.FsRepo',
-		'location': SPEC_DATA_DIR,
-	},
-	'CAPABILITIES': {
-		'version_control': True,
-		'create_projects': True,
-	}
-}
-'''
-#################### DASH #####################################
-AUTH_CONFIG = {
-	'CALLABLE': 'slyd.dash.dashauth.protectResource',
-	'CONFIG': {
-		'dash_url': DASH_API_URL,
-	}
-}
-
-SPEC_FACTORY = {
-	'PROJECT_SPEC': 'slyd.dash.projectspec.ProjectSpec',
-	'PROJECT_MANAGER': 'slyd.dash.projects.ProjectsManager',
-	'PARAMS': {
-		'storage_backend': 'dulwich.mysqlrepo.MysqlRepo',
-		'location': MYSQL_DB,
-		'dash_url': DASH_API_URL,
-	},
-	'CAPABILITIES': {
-		'version_control': True,
-		'create_projects': False,
-		'rename_projects': False,
-		'deploy_projects': True,
+		'rename_projects': True,
+		'deploy_projects': False,
 	}
 }
 
