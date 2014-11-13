3fa4f0d14f6137f620ee22516dd7571b86935e19
duendex <duendex@gmail.com>
2014-11-13 16:55:16 -0200

    Updated restore-mtime

--- docker/restore-mtime.sh
+++ docker/restore-mtime.sh
@@ -3,6 +3,6 @@ commit=$(git rev-list -n 1 HEAD slyd/requirements.txt)
 mtime=$(git show --pretty=format:%ai --abbrev-commit $commit |head -n1)
 touch -d "$mtime" slyd/requirements.txt
 
-commit=$(git rev-list -n 1 HEAD /slybot)
+commit=$(git rev-list -n 1 HEAD .slybot)
 mtime=$(git show --pretty=format:%ai --abbrev-commit $commit |head -n1)
-touch -d "$mtime" /slybot
+touch -d "$mtime" .slybot
