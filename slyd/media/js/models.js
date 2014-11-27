76ccc78555f7945e1f7d69e2788cbaa2c81b0e9b
duendex <duendex@gmail.com>
2014-11-27 13:34:12 -0200

    Validate all item/field names before saving.

--- slyd/media/js/models.js
+++ slyd/media/js/models.js
@@ -386,6 +386,18 @@ ASTool.Item = ASTool.SimpleModel.extend({
 	serializedRelations: ['fields'],
 	serializedProperties: ['name'],
 	fields: null,
+
+	validateName: function(name) {
+		return /^[A-Za-z0-9_-]+$/g.test(name);
+	},
+
+	isValid: function() {
+		return this.validateName(this.get('name')) && this.get('fields').reduce(
+			function(previousValue, field){
+				return previousValue && this.validateName(field.get('name'));
+			}.bind(this), true
+		);
+	},
 });
 
 
