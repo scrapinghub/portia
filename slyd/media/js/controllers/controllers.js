426ddd8579c980d90ebd3ff490a7b182b87e7a99
Ruairi Fahy <ruairi@scrapinghub.com>
2015-01-21 11:57:15 +0000

    Add better responsive layout support

--- slyd/media/js/controllers/controllers.js
+++ slyd/media/js/controllers/controllers.js
@@ -1,5 +1,46 @@
 ASTool.BaseControllerMixin = Ember.Mixin.create({
 	__modal_content: null,
+	full_box_height: 600,
+	mid_box_height: 500,
+	small_box_height: 400,
+	tiny_box_height: 300,
+	ex_tiny_box_height: 200,
+
+	handleResize: function() {
+		$('.adjust-height').height(window.innerHeight - 38);
+		this.set('full_box_height', Math.max(80, window.innerHeight - 200));
+		this.set('mid_box_height', Math.max(80, window.innerHeight - 300));
+		this.set('small_box_height', Math.max(80, window.innerHeight - 400));
+		this.set('tiny_box_height', Math.max(80, window.innerHeight - 500));
+		this.set('ex_tiny_box_height', Math.max(60, window.innerHeight - 600));
+	},
+
+	full_box_style: function() {
+		return 'max-height: ' + this.full_box_height + 'px;';
+	}.property('full_box_height'),
+
+	mid_box_style: function() {
+		return 'max-height: ' + this.mid_box_height + 'px;';
+	}.property('mid_box_height'),
+
+
+	small_box_style: function() {
+		return 'max-height: ' + this.small_box_height + 'px;';
+	}.property('small_box_height'),
+
+
+	tiny_box_style: function() {
+		return 'max-height: ' + this.tiny_box_height + 'px;';
+	}.property('tiny_box_height'),
+
+	ex_tiny_box_style: function() {
+		return 'max-height: ' + this.ex_tiny_box_height + 'px;';
+	}.property('ex_tiny_box_height'),
+
+	bindResizeEvent: function() {
+		this.handleResize();
+		jQuery(window).on('resize', Ember.run.bind(this, this.handleResize));
+	}.on('init'),
 
 	openAccordion: function(accordionNumber) {
 		$( ".accordion" ).accordion("option", "active", accordionNumber);
