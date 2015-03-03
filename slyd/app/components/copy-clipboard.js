e6c00607dd9b12e3126bcdc5b3ca4b9d16fdcecf
Ruairi Fahy <ruairi@scrapinghub.com>
2015-03-03 16:50:12 +0000

    Move from clippy.swf to ZeroClipboard

--- slyd/app/components/copy-clipboard.js
+++ slyd/app/components/copy-clipboard.js
@@ -1,22 +1,14 @@
-import Ember from 'ember';
+import ZeroClipboard from 'ember-cli-zero-clipboard/components/zero-clipboard';
 
-export default Ember.Component.extend({
-    tagName: 'embed',
-    text: '',
-    src: 'assets/clippy.swf',
-    width:"14",
-    height: "14",
-    scale: "noscale",
-    name: "clippy",
-    quality: "high",
-    allowScriptAccess: "always",
-    type: "application/x-shockwave-flash",
-    pluginspage: "http://www.macromedia.com/go/getflashplayer",
+export default ZeroClipboard.extend({
+    layoutName: 'components/bs-button',
+    tagName: 'button',
+    classNameBindings: 'class',
+    class: 'btn btn-default btn-xs',
+    icon: 'fa fa-icon fa-clipboard',
 
-    flashvars: function() {
-        return 'text=' + this.get('text');
-    }.property('text'),
-
-    attributeBindings: ['src', 'width', 'height', 'name', 'quality', 'allowScriptAccess',
-        'type', 'pluginspage', 'flashvars', 'scale'],
+    actions: {
+        afterCopy: function() {
+        }
+    }
 });
