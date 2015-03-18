e89066f4bb2bc70bd3408847a4857286501312e7
Ruairi Fahy <ruairi@scrapinghub.com>
2015-03-18 09:08:03 +0000

    Add templates when compiling egg

--- slyd/slyd/projecttemplates.py
+++ slyd/slyd/projecttemplates.py
@@ -33,7 +33,7 @@ setup(
     version      = '1.0',
     packages     = find_packages(),
     package_data = {
-        'spiders': ['*.json']
+        'spiders': ['*.json', '*/*.json']
     },
     data_files = [('', ['project.json', 'items.json', 'extractors.json'])],
     entry_points = {'scrapy': ['settings = spiders.settings']},
