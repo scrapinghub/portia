9c77443808e8f682598758c9265c340f2f675990
Ruairi Fahy <ruairi@scrapinghub.com>
2015-04-15 11:48:07 +0100

    Update accordion css. Update copy to clipboard button css.

--- slyd/app/components/copy-clipboard.js
+++ slyd/app/components/copy-clipboard.js
@@ -4,7 +4,7 @@ export default ZeroClipboard.extend({
     layoutName: 'components/bs-button',
     tagName: 'button',
     classNameBindings: 'class',
-    class: 'btn btn-default btn-xs',
+    class: 'btn btn-default btn-xs btn-xs-size fa fa-icon fa-clipboard',
     icon: 'fa fa-icon fa-clipboard',
 
     actions: {
