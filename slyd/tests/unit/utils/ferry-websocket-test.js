efdcf4eab767f0feba6d30224a01887aef20cdf6
Ruairi Fahy <ruairi@scrapinghub.com>
2015-05-08 10:47:00 +0100

    Connect to websocket on page load. Reload connection on close. Close connection on unload

---
+++ slyd/tests/unit/utils/ferry-websocket-test.js
@@ -0,0 +1,9 @@
+import ferryWebsocket from 'portia-web/utils/ferry-websocket';
+
+module('ferryWebsocket');
+
+// Replace this with your real tests.
+test('it works', function() {
+  var result = ferryWebsocket();
+  ok(result);
+});
