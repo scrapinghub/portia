3dcb500fe7fca98afd8716aa99ff6eebe843b9c3
Ruairi Fahy <ruairi@scrapinghub.com>
2015-05-21 13:07:56 +0100

    Fix regex validation

--- slyd/app/components/inline-editable-text-field.js
+++ slyd/app/components/inline-editable-text-field.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend(NotificationHandler, {
     tagName: 'div',
     classNames: 'class',
     editing: false,
-    validation: '*',
+    validation: '.*',
     text: '',
     name: null,
 
---
+++ slyd/tests/unit/components/regex-text-field-with-button/component-test.js
@@ -0,0 +1,18 @@
+import { moduleForComponent, test } from 'ember-qunit';
+
+moduleForComponent('regex-text-field-with-button', 'Unit | Component | regex text field with button', {
+  // Specify the other units that are required for this test
+  // needs: ['component:foo', 'helper:bar']
+});
+
+test('it renders', function(assert) {
+  assert.expect(2);
+
+  // Creates the component instance
+  var component = this.subject();
+  assert.equal(component._state, 'preRender');
+
+  // Renders the component to the page
+  this.render();
+  assert.equal(component._state, 'inDOM');
+});
