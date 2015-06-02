bf015a36c9d1f6a1292e1bfa662d9a3ffbc65915
Ruairi Fahy <ruairi@scrapinghub.com>
2015-06-02 17:18:41 +0100

    Add new `projects/PID/items` route. Edit project items from dropdown on project overview page

---
+++ slyd/app/controllers/template-items.js
@@ -0,0 +1,3 @@
+import Items from './items';
+
+export default Items;
---
+++ slyd/app/routes/template-items.js
@@ -0,0 +1,5 @@
+import Items from './items';
+
+export default Items.extend({
+    defaultControllerName: 'template-items',
+});
