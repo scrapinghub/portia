5a00fbb2fb177acbcff0dfb0a3cda4a09653ebab
Michał Modzelewski <michal.modzelewski@gmail.com>
2015-06-09 12:16:35 +0200

    make portia frontend an ember plugin

--- slyd/Brocfile.js
+++ slyd/Brocfile.js
@@ -1,55 +1,10 @@
 /* global require, module */
-var mergeTrees = require('broccoli-merge-trees');
-var pickFiles = require('broccoli-static-compiler');
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
-    vendorFiles: {
-        'handlebars.js': null
+    fingerprint: {
+        exclude: ['scrapinghub-logo.png']
     }
 });
 
-// Use `app.import` to add additional libraries to the generated
-// output files.
-//
-// If you need to use different assets in different
-// environments, specify an object as the first parameter. That
-// object's keys should be the environment name and the values
-// should be the asset to use in that environment.
-//
-// If the library that you are including contains AMD or ES6
-// modules that you would like to import into your application
-// please specify an object with the list of modules as keys
-// along with the exports of each module as its value.
-
-app.import('bower_components/babel-polyfill/browser-polyfill.js');
-app.import('bower_components/ic-ajax/dist/named-amd/main.js');
-app.import('bower_components/canvasloader/js/heartcode-canvasloader-min.js');
-app.import('vendor/uri.js');
-app.import('vendor/bootstrap.min.js');
-app.import('vendor/jquery.binarytransport.js');
-
-app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
-app.import('bower_components/bootstrap/dist/css/bootstrap-theme.min.css');
-app.import('bower_components/normalize.css/normalize.css');
-app.import('app/styles/jquery-ui-1.10.3.custom.css');
-app.import('bower_components/fontawesome/css/font-awesome.min.css');
-
-if (app.env !== 'production') {
-    // For testing
-    app.import('bower_components/ember/ember-template-compiler.js');
-    app.import('vendor/test-loader.js');
-}
-
-var fontTree = pickFiles('bower_components/fontawesome/fonts', {
-    srcDir: '/',
-    files: ['fontawesome-webfont.eot','fontawesome-webfont.ttf','fontawesome-webfont.svg','fontawesome-webfont.woff', 'fontawesome-webfont.woff2'],
-    destDir: '/assets/fonts'
-});
-
-var publicFiles = pickFiles('public', {
-    srcDir: '/',
-    destDir: '/',
-});
-
-module.exports = mergeTrees([app.toTree(), fontTree, publicFiles], {overwrite: true});
+module.exports = app.toTree();
