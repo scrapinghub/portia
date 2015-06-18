a5fcfa5ad571ddd16b8a4d0e06ff9bedeeadaa6a
David Bengoa <david@bengoarocandio.com>
2015-06-18 18:10:56 +0700

    Fix two more tests

--- slyd/tests/unit/components/top-bar-test.js
+++ slyd/tests/unit/components/top-bar-test.js
@@ -5,14 +5,19 @@ import {
 
 moduleForComponent('top-bar', 'TopBarComponent', {
   // specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
+   needs: ['component:scrapinghub-branding']
 });
 
 test('it renders', function() {
   expect(2);
 
   // creates the component instance
-  var component = this.subject();
+  var component = this.subject({
+    branding: {
+      component: 'scrapinghub-branding',
+      data: {}
+    }
+  });
   equal(component._state, 'preRender');
 
   // appends the component to the page
