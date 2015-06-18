4db61aa953cb6aa23d28bb58c5d9a52947f7215d
David Bengoa <david@bengoarocandio.com>
2015-06-18 17:37:39 +0700

    Removed some tests of unexistent modules or broken

--- slyd/tests/unit/utils/messages-test.js
+++
@@ -1,9 +0,0 @@
-import messages from 'portia-web/utils/messages';
-
-module('messages');
-
-// Replace this with your real tests.
-test('it works', function() {
-  var result = messages();
-  ok(result);
-});
