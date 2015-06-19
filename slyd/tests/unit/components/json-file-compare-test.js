cab95383dead4d723899551461afc2b0865b2f7a
David Bengoa <david@bengoarocandio.com>
2015-06-19 16:08:10 +0700

    Added more tests

--- slyd/tests/unit/components/json-file-compare-test.js
+++ slyd/tests/unit/components/json-file-compare-test.js
@@ -4,18 +4,16 @@ import {
 } from 'ember-qunit';
 
 moduleForComponent('json-file-compare', 'JsonFileCompareComponent', {
-  // specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
 });
 
 test('it renders', function() {
-  expect(2);
-
   // creates the component instance
-  var component = this.subject();
-  equal(component._state, 'preRender');
 
-  // appends the component to the page
-  this.append();
+  var component = this.subject({
+    json: {foo: 5, bar:'foo'},
+  });
+  equal(component._state, 'preRender');
+  this.render();
   equal(component._state, 'inDOM');
 });
+
