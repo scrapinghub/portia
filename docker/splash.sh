ec6d39fff298399eab8200ed9aa43b4e9ed0f9e5
Ruairi Fahy <ruairi@scrapinghub.com>
2015-06-25 15:30:10 +0100

    Update requirements

--- docker/splash.sh
+++ docker/splash.sh
@@ -14,26 +14,12 @@ apt-get install -qy --no-install-recommends netbase \
 
 # Install python dependencies
 pip install --no-cache-dir \
-        Twisted==15.1.0 \
+        Twisted==15.2.0 \
         qt4reactor==1.6 \
         psutil==2.2.1 \
         adblockparser==0.4 \
-        https://github.com/axiak/pyre2/archive/master.zip#egg=re2 \
+        re2==0.2.22 \
         xvfbwrapper==0.2.4 \
         lupa==1.1 \
         funcparserlib==0.3.6 \
         Pillow==2.8.1
-
-# Remove additional installed packages and files to reduce container size
-# apt-get remove -y --purge \
-#         libre2-dev \
-#         liblua5.2-dev \
-#         zlib1g-dev \
-#         perl && \
-#     apt-get autoremove -y && \
-#     apt-get clean -y && \
-# rm -rf /usr/share/perl \
-#        /usr/share/perl5 \
-#        /usr/share/man \
-#        /usr/share/info \
-#        /usr/share/doc
