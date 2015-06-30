706d34bd2c2e78c8a05b2499626c2a1f0332c634
Ruairi Fahy <ruairi@scrapinghub.com>
2015-06-30 12:40:37 +0100

    Update slybot to use splash middleware

--- slybot/slybot/settings.py
+++ slybot/slybot/settings.py
@@ -2,6 +2,7 @@ SPIDER_MANAGER_CLASS = 'slybot.spidermanager.SlybotSpiderManager'
 EXTENSIONS = {'slybot.closespider.SlybotCloseSpider': 1}
 ITEM_PIPELINES = {'slybot.dupefilter.DupeFilterPipeline': 1}
 SPIDER_MIDDLEWARES = {'slybot.spiderlets.SpiderletsMiddleware': 999}  # as close as possible to spider output
+DOWNLOADER_MIDDLEWARES = {'scrapyjs.SplashMiddleware': 725}
 PLUGINS = [
     'slybot.plugins.scrapely_annotations.Annotations',
     'slybot.plugins.selectors.Selectors'
