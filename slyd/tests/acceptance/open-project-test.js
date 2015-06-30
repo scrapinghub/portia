a349c6e79057dc133e89ff3b068e8d7c66e67c10
David Bengoa <david@bengoarocandio.com>
2015-06-30 15:38:17 +0700

    Removed branding from fixtures, removed conflict redirect test

--- slyd/tests/acceptance/open-project-test.js
+++ slyd/tests/acceptance/open-project-test.js
@@ -1,7 +1,6 @@
 
 import acceptanceTest from '../helpers/acceptance-test';
 import Ember from 'ember';
-import ajax from 'ic-ajax';
 
 module('Acceptance | Open Projects', { });
 
@@ -14,17 +13,6 @@ acceptanceTest('Open a project', function(app, assert) {
     return click(projectLinks[0]);
   }).then(function(){
     equal(currentURL(), '/projects/11');
-  });
-});
-
-acceptanceTest('Open a project with conflicts', function(app, assert) {
-  visit('/').then(function(){
-    equal(currentURL(), '/projects');
-    var projectLinks = find('.clickable-url button');
-    equal(projectLinks.length, 2, 'There are two projects');
-    return click(projectLinks[1]);
-  }).then(function(){
-    equal(currentURL(), '/projects/12/conflicts');
   });
 });
 
