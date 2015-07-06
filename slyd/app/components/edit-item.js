92f50178d676119112292d5f6c6382e0086ff009
David Bengoa <david@bengoarocandio.com>
2015-07-06 12:33:24 +0700

    Make sure field names start with a letter
    So that properties like _item and _template and any we might add later to the
    extracted item can't be overwritten.

--- slyd/app/components/edit-item.js
+++ slyd/app/components/edit-item.js
@@ -26,13 +26,19 @@ export default Ember.Component.extend(NotificationHandler, {
         },
 
         editField: function(text, index) {
-            if (text === 'url') {
+            // Ensuring that field names start with a letter prevents overwriting
+            // _item, _template and any future "protected" property we might
+            // add to extracted items.
+            if (text === 'url' || !/^[a-z]/i.test(text)) {
                 var field = this.get('item.fields').get(index);
                 if (field) {
                     field.set('name', this.get('itemFields').get(index).name);
                     this.get('item.fields').replace(index, 1, [field]);
                 }
-                this.showErrorNotification('Naming a field "url" is not allowed as there is already a field with this name');
+                this.showErrorNotification(text === 'url' ?
+                    'Naming a field "url" is not allowed as there is already a field with this name' :
+                    'Field names must start with a letter'
+                );
                 return;
             }
             this.updateFields();
