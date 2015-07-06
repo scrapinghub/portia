5a6cfaea5964d96293b70d3c41589355a0620ba9
David Bengoa <david@bengoarocandio.com>
2015-07-06 21:07:40 +0700

    Refactored item validations, added duplicate item name validation

--- slyd/app/components/edit-item.js
+++ slyd/app/components/edit-item.js
@@ -3,52 +3,40 @@ import NotificationHandler from '../mixins/notification-handler';
 
 export default Ember.Component.extend(NotificationHandler, {
     item: null,
-    itemFields: null,
+    items: null,
     extractionTypes: [],
-
-    updateFields: function() {
-        this.set('itemFields', (this.getWithDefault('item.fields', []) || []).copy());
-    }.on('init'),
 
     actions: {
         addField: function() {
             this.sendAction('addField', this.get('item'));
-            this.updateFields();
         },
 
         deleteField: function(field) {
             this.sendAction('deleteField', this.get('item'), field);
-            this.updateFields();
         },
 
         delete: function() {
             this.sendAction('delete', this.get('item'));
         },
 
-        editField: function(text, index) {
-            var error = null;
+        validateFieldName: function(input) {
+            var text = input.text;
 
             // Ensuring that field names start with a letter prevents overwriting
             // _item, _template and any future "protected" property we might
             // add to extracted items.
             if (!/^[a-z]/i.test(text)) {
-                error = 'Field names must start with a letter';
+                return input.setInvalid('Field names must start with a letter');
             } else if (text === 'url') {
-                error = 'Naming a field "url" is not allowed as there is already a field with this name';
+                return input.setInvalid('Naming a field "url" is not allowed as there is already a field with this name');
             } else if (this.get('item.fields').findBy('name', text)) {
-                error = 'There is alrready a field with that name.';
+                return input.setInvalid('There is already a field with that name.');
             }
+        },
 
-            if(error) {
-                // Reset name to it's original value
-                var field = this.get('item.fields').get(index);
-                if (field) {
-                    field.set('name', this.get('itemFields').get(index).name);
-                    this.get('item.fields').replace(index, 1, [field]);
-                }
-                this.showErrorNotification(error);
-            } else {
-                this.updateFields();
+        validateItemName: function(input){
+            if (this.get('items').findBy('name', input.text)) {
+                input.setInvalid('There is already a item with that name.');
             }
         }
     }
