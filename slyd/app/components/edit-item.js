cf9598c3c12f3d8751e8ffa25e02b5dad0796656
David Bengoa <david@bengoarocandio.com>
2015-07-06 12:56:20 +0700

    Prevent two fields from having the same field name

--- slyd/app/components/edit-item.js
+++ slyd/app/components/edit-item.js
@@ -26,22 +26,30 @@ export default Ember.Component.extend(NotificationHandler, {
         },
 
         editField: function(text, index) {
+            var error = null;
+
             // Ensuring that field names start with a letter prevents overwriting
             // _item, _template and any future "protected" property we might
             // add to extracted items.
-            if (text === 'url' || !/^[a-z]/i.test(text)) {
+            if (!/^[a-z]/i.test(text)) {
+                error = 'Field names must start with a letter';
+            } else if (text === 'url') {
+                error = 'Naming a field "url" is not allowed as there is already a field with this name';
+            } else if (this.get('item.fields').findBy('name', text)) {
+                error = 'There is alrready a field with that name.';
+            }
+
+            if(error) {
+                // Reset name to it's original value
                 var field = this.get('item.fields').get(index);
                 if (field) {
                     field.set('name', this.get('itemFields').get(index).name);
                     this.get('item.fields').replace(index, 1, [field]);
                 }
-                this.showErrorNotification(text === 'url' ?
-                    'Naming a field "url" is not allowed as there is already a field with this name' :
-                    'Field names must start with a letter'
-                );
-                return;
+                this.showErrorNotification(error);
+            } else {
+                this.updateFields();
             }
-            this.updateFields();
         }
     }
 });
