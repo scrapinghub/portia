80af16f38a63ef00608e9a98986a737c359c5a74
Michał Modzelewski <michal.modzelewski@gmail.com>
2015-09-24 14:27:25 +0200

    reworked items panel

--- portiaui/app/components/extracted-item-table.js
+++ portiaui/app/components/extracted-item-table.js
@@ -1,11 +1,7 @@
 import Ember from 'ember';
-import utils from '../utils/utils';
+
 
 export default Ember.Component.extend({
-    tagName: Ember.computed('item', function() {
-        var item = this.get('item'),
-            isArrayOrObject = Array.isArray(item) || utils.toType(item) === 'object'
-        return isArrayOrObject ? 'table' : '';
-    }),
+    tagName: 'table',
     classNames: ['extracted-item-table']
 });
---
+++ portiaui/tests/unit/helpers/is-empty-object-test.js
@@ -0,0 +1,10 @@
+import { isEmptyObject } from '../../../helpers/is-empty-object';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | is empty object');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = isEmptyObject(42);
+  assert.ok(result);
+});
