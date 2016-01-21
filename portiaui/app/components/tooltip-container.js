6751c8be5993fa3544ec59de5ed471a5bb496a71
Michał Modzelewski <michal.modzelewski@gmail.com>
2016-01-21 08:57:05 +0100

    * renamed start URLs to start pages * smaller start url button

--- portiaui/app/components/tooltip-container.js
+++ portiaui/app/components/tooltip-container.js
@@ -11,7 +11,7 @@ export default Ember.Component.extend({
     text: false,
     tooltipContainer: false,
     tooltipFor: false,
-    triggerEvents: 'hover focus',
+    triggerEvents: 'hover',
     viewport: {
         selector: 'body',
         padding: 0
