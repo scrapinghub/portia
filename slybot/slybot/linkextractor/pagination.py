cc361bb5b690d08efd4d4d117eae14e0fbd45652
Pedro Lopez-Adeva <plopezadeva@gmail.com>
2016-01-28 15:03:33 +0100

    Added pagination link extractor

---
+++ slybot/slybot/linkextractor/pagination.py
@@ -0,0 +1,27 @@
+from page_finder import LinkAnnotation
+from .html import HtmlLinkExtractor
+
+
+class PaginationExtractor(HtmlLinkExtractor):
+    def __init__(self):
+        self.link_annotation = LinkAnnotation()
+        self.visited = set()
+        super(PaginationExtractor, self).__init__()
+
+    def _extract_links(self, response):
+        self.visited.add(response.url)
+        url_to_link = {
+            url: link
+            for link in super(PaginationExtractor, self)._extract_links(response)
+        }
+        self.link_annotation.load(url_to_link)
+        n_items = response.meta.get('n_items')
+        if pagination is not None:
+            self.link_annotation.mark_link(response.url, follow=(n_items > 0))
+        best = self.link_annotation.best_links_to_follow()
+        if best:
+            for url in best:
+                if url not in self.visited:
+                    return url_to_link[url] # TODO: extract only the best link?
+        else:
+            return url_to_link.values()
