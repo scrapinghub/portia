f4595e08ffcd03203c213ff59ad32e0cf6cc65f3
Ruairi Fahy <ruairifahy91@gmail.com>
2016-02-25 17:23:21 +0000

    343 Unexpected error when renaming schema

--- portiaui/app/models/schema.js
+++ portiaui/app/models/schema.js
@@ -1,10 +1,8 @@
 import DS from 'ember-data';
 
-const Schema = DS.Model.extend({
+export default DS.Model.extend({
     name: DS.attr('string'),
     project: DS.belongsTo(),
     fields: DS.hasMany(),
     items: DS.hasMany()
 });
-
-export default Schema;
