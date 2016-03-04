0e7b50498280daef5e46cadd458c4c9e1af8c01d
Michał Modzelewski <michal.modzelewski@gmail.com>
2016-03-04 15:52:18 +0100

    retry if fetching server capabilities fails

--- portiaui/app/services/capabilities.js
+++ portiaui/app/services/capabilities.js
@@ -2,11 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
     ajax: Ember.inject.service(),
-    
-    init() {
-        this._super(...arguments);
+
+    fetchCapabilities: Ember.on('init', function() {
         this.get('ajax').request('/server_capabilities').then(capabilities => {
             this.setProperties(capabilities);
+        }, () => {
+            Ember.run.later(this, this.fetchCapabilities, 5000);
         });
-    }
+    })
 });
