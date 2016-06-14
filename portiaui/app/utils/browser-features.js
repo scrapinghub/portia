42b5e5fab3d565c4cb3067a002b3d38a3f8bfc1c
Pablo Villegas <pvillegas@appsinfinita.com>
2016-06-14 07:17:25 -0500

    PORTIA-536: Disable generatedcontent detection

--- portiaui/app/utils/browser-features.js
+++ portiaui/app/utils/browser-features.js
@@ -2,9 +2,10 @@ import Ember from 'ember';
 const { RSVP } = Ember;
 
 export default function hasBrowserFeatures() {
+    // generatedcontent: detection issue with zoom in chrome
     let features = [
         "eventlistener", "json", "postmessage", "queryselector", "requestanimationframe", "svg",
-        "websockets", "cssanimations", "csscalc", "flexbox", "generatedcontent", "nthchild",
+        "websockets", "cssanimations", "csscalc", "flexbox", "nthchild",
         "csspointerevents", "opacity", "csstransforms", "csstransitions", "cssvhunit",
         "classlist", "placeholder", "localstorage", "svgasimg", "datauri", "atobbtoa"
     ];
