cd0a700ca61e7d247211f971037e1c42af42a0d2
Pablo Villegas <pvillegas12@gmail.com>
2016-06-27 10:01:06 -0500

    PORTIA-539 Start URLs for Slybot

---
+++ slybot/slybot/starturls/fragment_generator.py
@@ -0,0 +1,27 @@
+from itertools import product
+
+import six
+
+
+class FragmentGenerator(object):
+    def _process_fixed(self, fragment):
+        return [fragment]
+
+    def _process_list(self, fragment):
+        return fragment.split(' ')
+
+    def _process_range(self, fragment):
+        a, b = map(int, fragment.split('-'))
+        return (str(i) for i in six.moves.range(a, b + 1))
+
+    def _process_fragment(self, fragment):
+        processor = getattr(self, '_process_{}'.format(fragment['type']))
+        return processor(fragment['value'])
+
+    def process_fragments(self, spec):
+        return map(self._process_fragment, spec['fragments'])
+
+    def __call__(self, spec):
+        generated = product(*self.process_fragments(spec))
+        for fragment_list in generated:
+            yield ''.join(fragment_list)
--- slybot/slybot/tests/test_schema_validation.py
+++ slybot/slybot/tests/test_schema_validation.py
@@ -10,6 +10,17 @@ from slybot.utils import open_project_from_dir
 
 _TEST_PROJECT_DIR = join(dirname(__file__), "data/SampleProject")
 
+def spider_json(start_urls):
+    return {
+        "start_urls": start_urls,
+        "links_to_follow": "none",
+        "respect_nofollow": True,
+        "templates": [],
+    }
+
+def start_url_schema(url_string):
+    return {"url": url_string, "type": "url"}
+
 class JsonSchemaTest(TestCase):
 
     def test_regex_formatting_wrong(self):
@@ -32,53 +43,78 @@ class JsonSchemaTest(TestCase):
         self.assertEqual(validator.validate(obj), None)
 
     def test_valid_url(self):
-        obj = {
-            "start_urls": [
-                'http://www.example.com/',
-                'http://www.example.com/經濟',
-                'http://www.example.com/?q=經濟',
-                'http://www.example.com/#經濟',
-                'http://faß.de',
-                'http://例.jp/',
-                'http://[2001:0000:1234:0000:0000:C1C0:ABCD:0876]/foo/bar',
-                'http://[2001::]/foo/bar',
-                'http://8.8.8.8/foo/bar',
-                'http://localhost:8080/foo/bar',
-                'http://foo@localhost:8080/foo/bar',
-                'http://foo:bar@localhost:8080/foo/bar',
-                'http://domain.com/path/file.html?param=FOO^111&param2=bar&param3=true&_param4=on', # Anonymized URL form sentry d46840d2457c4042b1b58f2fa40e984b
-                'https://domain.com/path/file.htm?param=foo#hash/foo/bar/baz:foo|bar:baz',          # Anonymized URL from sentry 01dd2fa09d9540b69ebd33372b2b3a2d
-                'https://domain.com/path/file.htm?param=foo#hash/foo/bar/baz:foo|bar%5B%5D:12345',  # Anonymized URL from sentry 87d49ee751494c90a8941dcbdacea634
-                'http://domain.com/path?bar[foo]=baz&foo[bar]=12345',                               # Anonymized URL from sentry 9f6835f5decd4d57b9475f04f0a58bd4
+        legacy_start_urls = [
+            'http://www.example.com/',
+            'http://www.example.com/經濟',
+            'http://www.example.com/?q=經濟',
+            'http://www.example.com/#經濟',
+            'http://faß.de',
+            'http://例.jp/',
+            'http://[2001:0000:1234:0000:0000:C1C0:ABCD:0876]/foo/bar',
+            'http://[2001::]/foo/bar',
+            'http://8.8.8.8/foo/bar',
+            'http://localhost:8080/foo/bar',
+            'http://foo@localhost:8080/foo/bar',
+            'http://foo:bar@localhost:8080/foo/bar',
+            'http://domain.com/path/file.html?param=FOO^111&param2=bar&param3=true&_param4=on', # Anonymized URL form sentry d46840d2457c4042b1b58f2fa40e984b
+            'https://domain.com/path/file.htm?param=foo#hash/foo/bar/baz:foo|bar:baz',          # Anonymized URL from sentry 01dd2fa09d9540b69ebd33372b2b3a2d
+            'https://domain.com/path/file.htm?param=foo#hash/foo/bar/baz:foo|bar%5B%5D:12345',  # Anonymized URL from sentry 87d49ee751494c90a8941dcbdacea634
+            'http://domain.com/path?bar[foo]=baz&foo[bar]=12345',                               # Anonymized URL from sentry 9f6835f5decd4d57b9475f04f0a58bd4
+        ]
 
-            ],
-            "links_to_follow": "none",
-            "respect_nofollow": True,
-            "templates": [],
-        }
+        start_urls = map(start_url_schema, legacy_start_urls)
         validator = get_schema_validator("spider")
-        self.assertEqual(validator.validate(obj), None)
+
+        self.assertEqual(validator.validate(spider_json(legacy_start_urls)), None)
+        self.assertEqual(validator.validate(spider_json(start_urls)), None)
 
     def test_invalid_url(self):
-        for invalid_url in (
-                12345, # Not a string
-                'example.com', # Lacks protocol
-                'http://[:::1]/foo/bar', # Bad IPv6 addr
-                'http://http://foo.com/bar', # Double protocol
-                'spotify:foobar', # Not http/s protocol
-                '/foo', # relative
-                '?foo', # relative
-                '#foo', # relative
-            ):
-            obj = {
-                "start_urls": [invalid_url],
-                "links_to_follow": "none",
-                "respect_nofollow": True,
-                "templates": [],
-            }
-            validator = get_schema_validator("spider")
+        legacy_start_urls = (
+            12345, # Not a string
+            'example.com', # Lacks protocol
+            'http://[:::1]/foo/bar', # Bad IPv6 addr
+            'http://http://foo.com/bar', # Double protocol
+            'spotify:foobar', # Not http/s protocol
+            '/foo', # relative
+            '?foo', # relative
+            '#foo', # relative
+        )
+
+        validator = get_schema_validator("spider")
+        for invalid_url in legacy_start_urls:
             with self.assertRaises(ValidationError):
-                validator.validate(obj)
+                validator.validate(spider_json([invalid_url]))
+
+    def test_valid_mixed_fragments(self):
+        start_urls = [
+            {'type': 'url', 'url': 'http://www.example.com/'},
+            {'type': 'generated', 'url': 'http://', 'fragments': [
+                {'type': 'fixed', 'value': 'http://'}
+            ]},
+        ]
+        validator = get_schema_validator("spider")
+
+        self.assertEqual(validator.validate(spider_json(start_urls)), None)
+
+    def test_valid_fragments(self):
+        fragments = (
+            [
+                {'type': 'fixed', 'value': 'domain.com'},
+                {'type': 'range', 'value': '0-10'}
+            ],
+            [{'type': 'range', 'value': '0-10'}],
+            [{'type': 'list', 'value': 'a b c'}],
+            [{'type': 'list', 'value': 'one_element'}]
+        )
+
+        validator = get_schema_validator("spider")
+        for fragment in fragments:
+            start_url = [{
+                'url': 'http://domain.com',
+                'type': 'generated',
+                'fragments': fragment
+            }]
+            self.assertEqual(validator.validate(spider_json(start_url)), None)
 
     def test_test_project(self):
         specs = open_project_from_dir(_TEST_PROJECT_DIR)
