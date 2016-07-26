dba31833025920d2a082573e44c3099b43c32932
Ruairi Fahy <ruairifahy91@gmail.com>
2016-07-26 17:07:45 +0100

    Load images in splash.
    Handle cases where splash can be blocked while waiting for image response

--- slyd/slyd/splash/ferry.py
+++ slyd/slyd/splash/ferry.py
@@ -344,7 +344,7 @@ class FerryServerProtocol(WebSocketServerProtocol):
             self.populate_window_object
         )
 
-        self.tab.set_images_enabled(False)
+        self.tab.set_images_enabled(True)
         self.tab.set_viewport(meta.get('viewport') or _DEFAULT_VIEWPORT)
         self.tab.set_user_agent(meta.get('user_agent') or _DEFAULT_USER_AGENT)
         self.tab.loaded = False
