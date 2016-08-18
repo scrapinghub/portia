9d302aa5f2373aa4cc8eed1297915cae6992c692
joygeorge <joy@particle41.com>
2016-08-18 21:46:12 +0530

    1. added after installation hook

--- start_server.sh
+++ start_server.sh
@@ -2,6 +2,6 @@
 
 echo "Starting slyd service"
 echo "====================="
-cd /home/ubuntu/portia/slyd && nohup bin/slyd -p 9002 &
+cd /home/ubuntu/portia/slyd && nohup bin/slyd -p 9002 > /dev/null 2>&1 &
 sudo /etc/init.d/nginx restart
 exit 0
