975992038dfd342f05ab962becb5511761965bbf
joygeorge <joy@particle41.com>
2016-08-18 16:33:07 +0530

    1.Added scripts to start appliction

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Starting slyd service"
 echo "====================="
-#cd /home/ubuntu/portia/slyd && bin/slyd -p 9002
-#sudo /etc/init.d/nginx restart
+cd /home/ubuntu/portia/slyd && bin/slyd -p 9002
+sudo /etc/init.d/nginx restart
