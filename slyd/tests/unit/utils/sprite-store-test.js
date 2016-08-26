4d2dce1f77cf1cf350cfe8feb52dd081ecfaf7a7
Pablo Villegas <pvillegas12@gmail.com>
2016-08-26 17:56:07 -0500

    Fix CI Build

--- slyd/tests/unit/utils/sprite-store-test.js
+++
@@ -1,9 +0,0 @@
-import spriteStore from 'portia-web/utils/sprite-store';
-
-module('spriteStore');
-
-// Replace this with your real tests.
-test('it works', function() {
-  var result = spriteStore.create();
-  ok(result);
-});
