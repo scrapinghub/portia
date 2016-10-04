83e913e8ed9220a17728e3af53b1f7ec46beacee
Michał Modzelewski <michal.modzelewski@gmail.com>
2016-10-04 12:18:03 +0200

    update queries for compatibility with schema from django branch

--- slyd/slyd/gitstorage/repo.py
+++ slyd/slyd/gitstorage/repo.py
@@ -178,7 +178,8 @@ class MysqlObjectStore(BaseObjectStore):
         "HAS": "SELECT EXISTS(SELECT 1 FROM objs WHERE `oid`=%s AND `repo`=%s)",
         "ALL": "SELECT `oid` FROM objs WHERE `repo`=%s",
         "GET": "SELECT `type`, UNCOMPRESS(`data`) FROM objs WHERE `oid`=%s AND `repo`=%s",
-        "ADD": "INSERT IGNORE INTO objs values(%s, %s, %s, COMPRESS(%s), %s)",
+        "GET_SHORT": "SELECT `type`, UNCOMPRESS(`data`) FROM objs WHERE `oid` LIKE CONCAT(%s, '%%') AND `repo`=%s LIMIT 1",
+        "ADD": "INSERT IGNORE INTO objs (`oid`, `type`, `size`, `data`, `repo`) values(%s, %s, %s, COMPRESS(%s), %s)",
         "DEL": "DELETE FROM objs WHERE `oid`=%s AND `repo`=%s",
     }
 
@@ -350,7 +351,7 @@ class MysqlRefsContainer(RefsContainer):
         "DEL": "DELETE FROM `refs` WHERE `ref`=%s AND `repo`=%s",
         "ALL": "SELECT `ref` FROM `refs` WHERE `repo`=%s",
         "GET": "SELECT `value` FROM `refs` WHERE `ref` = %s AND `repo`=%s FOR UPDATE",
-        "ADD": "REPLACE INTO `refs` VALUES(%s, %s, %s)",
+        "ADD": "REPLACE INTO `refs` (`ref`, `value`, `repo`) VALUES(%s, %s, %s)",
     }
 
     def __init__(self, repo, connection):
