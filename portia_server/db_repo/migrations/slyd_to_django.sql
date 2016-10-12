d13d923517c9f01f01886125f317078e5f505706
Ruairi Fahy <ruairifahy91@gmail.com>
2016-10-12 09:36:43 +0100

    Update migration SQL to avoid unnecessary additional copies

--- portia_server/db_repo/migrations/slyd_to_django.sql
+++ portia_server/db_repo/migrations/slyd_to_django.sql
@@ -1,17 +1,17 @@
-ALTER TABLE `objs` DROP PRIMARY KEY;
-ALTER TABLE `objs` ADD COLUMN `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY;
-ALTER TABLE `objs` ADD CONSTRAINT `objs_oid_feda89ac_uniq` UNIQUE (`oid`, `repo`);
-ALTER TABLE `objs` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE `objs` ALTER COLUMN `oid` DROP DEFAULT;
+ALTER TABLE `objs` DROP PRIMARY KEY,
+                   ADD COLUMN `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                   ADD CONSTRAINT `objs_oid_feda89ac_uniq` UNIQUE (`oid`, `repo`),
+                   CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+                   ALTER COLUMN `oid` DROP DEFAULT;
 DROP INDEX `type` ON `objs`;
 DROP INDEX `size` ON `objs`;
 CREATE INDEX `objs_599dcce2` ON `objs` (`type`);
 CREATE INDEX `objs_f7bd60b7` ON `objs` (`size`);
 
-ALTER TABLE `refs` DROP PRIMARY KEY;
-ALTER TABLE `refs` ADD COLUMN `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY;
-ALTER TABLE `refs` ADD CONSTRAINT `refs_ref_4a751775_uniq` UNIQUE (`ref`, `repo`);
-ALTER TABLE `refs` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE `refs` ALTER COLUMN `ref` DROP DEFAULT;
+ALTER TABLE `refs` DROP PRIMARY KEY,
+                   ADD COLUMN `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                   ADD CONSTRAINT `refs_ref_4a751775_uniq` UNIQUE (`ref`, `repo`),
+                   CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+                   ALTER COLUMN `ref` DROP DEFAULT;
 DROP INDEX `value` ON `refs`;
 CREATE INDEX `refs_2063c160` ON `refs` (`value`);
