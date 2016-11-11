f6e958a10b07c6df00e3cf854fde825ee930f234
Pablo Villegas <pvillegas12@gmail.com>
2016-11-10 19:44:21 -0500

    Find Portia project through url

--- portiaui/app/adapters/project.js
+++ portiaui/app/adapters/project.js
@@ -2,5 +2,6 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
     urlTemplate: '{+host}/api/projects{/id}',
+    findRecordUrlTemplate: '{+host}/api/projects{/id}',
     createRecordUrlTemplate: '{+host}/api/projects'
 });
