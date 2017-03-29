cbb0c24c15b5823fd57674d18bd40a8bb94909b8
Ahmed Ezz Eldin <ahmedezeldin.93@gmail.com>
2017-03-29 12:46:05 +0200

    YCB-716: Add copying/pushing portia data dir to github

--- portia_server/portia_api/utils/train.py
+++ portia_server/portia_api/utils/train.py
@@ -1,5 +1,6 @@
 import os
 import json
+import shutil
 
 from django.core.exceptions import ValidationError
 from scrapy import log
@@ -9,6 +10,10 @@ from git import Repo
 
 KIPP_SETTINGS_REPO = '/app/kipp_settings'
 KIPP_MERCHANT_SETTINGS_DIR = '/app/kipp_settings/{country_code}/{spider_name}'
+PORTIA_DATA_DIR_BASE = '/app/slyd/slyd/data/projects/yaoota'
+PORTIA_SPIDERS_DIR = '/app/slyd/slyd/data/projects/yaoota/spiders'
+PORTIA_SPIDER_TEMPLATES = '/app/slyd/slyd/data/projects/yaoota/spiders/{spider_name}'
+
 
 def train_scrapely(storage, model, username):
     """
@@ -17,11 +22,12 @@ def train_scrapely(storage, model, username):
     :param model:
     :return:
     """
-    samples = load_samples(storage, model)
-    scrapely_templates = generate_scrapely_templates(samples)
-    save_scrapely_object(spider_name=model.id, country_code=model.country_code, username=username, scrapely_templates=scrapely_templates)
+    # samples = load_samples(storage, model)
+    # scrapely_templates = generate_scrapely_templates(samples)
+    # save_scrapely_object(spider_name=model.id, country_code=model.country_code, scrapely_templates=scrapely_templates)
     merchant_settings = create_kipp_setting(model)
-    save_kipp_config(spider_name=model.id, country_code=model.country_code, username=username, merchant_settings=merchant_settings)
+    save_kipp_config(spider_name=model.id, country_code=model.country_code, merchant_settings=merchant_settings)
+    save_portia_spider(spider_name=model.id, country_code=model.country_code)
     publish_kipp_settings(username=username, country_code=model.country_code, spider_name=model.id)
 
 
@@ -72,17 +78,46 @@ def generate_scrapely_templates(templates):
     return scrapely_templates
 
 
-def save_scrapely_object(spider_name, country_code, username, scrapely_templates):
+def save_portia_spider(spider_name, country_code):
+    """
+    Copy portia data for spider specified from data folder to kipp settings
+    :param spider_name:
+    :param country_code:
+    :return:
+    """
+    def copytree(src, dst, symlinks=False, ignore=None):
+        for item in os.listdir(src):
+            s = os.path.join(src, item)
+            d = os.path.join(dst, item)
+            if os.path.isdir(s):
+                copytree(s, d, symlinks, ignore)
+            else:
+                shutil.copy2(s, d)
+
+    kipp_merchant_settings_dir = KIPP_MERCHANT_SETTINGS_DIR.format(country_code=country_code, spider_name=spider_name)
+    if not os.path.exists(kipp_merchant_settings_dir):
+        os.makedirs(kipp_merchant_settings_dir)
+
+    portia_spider_name = "%s.json" % spider_name
+    portia_spider_file = os.path.join(PORTIA_SPIDERS_DIR, portia_spider_name)
+    merchant_file_path = os.path.join(kipp_merchant_settings_dir, portia_spider_name)
+    shutil.copyfile(portia_spider_file, merchant_file_path)
+    portia_spider_templates = PORTIA_SPIDER_TEMPLATES.format(spider_name=spider_name)
+    merchant_templates = os.path.join(kipp_merchant_settings_dir, "templates")
+    if not os.path.exists(merchant_templates):
+        os.makedirs(merchant_templates)
+    copytree(portia_spider_templates, merchant_templates)
+
+
+def save_scrapely_object(spider_name, country_code, scrapely_templates):
     """
     SaSaving spider configuration file for kipp on disk
     :param spider_name:
     :param country_code:
-    :param username:
     :param scrapely_templates:
     :return:
     """
-    kipp_merchant_settings_dir = KIPP_MERCHANT_SETTINGS_DIR.format(username=username, country_code=country_code,
-                                                                 spider_name=spider_name)
+    kipp_merchant_settings_dir = KIPP_MERCHANT_SETTINGS_DIR.format(country_code=country_code, spider_name=spider_name)
     if not os.path.exists(kipp_merchant_settings_dir):
         os.makedirs(kipp_merchant_settings_dir)
     scrapely_file_name = "%s.json" % spider_name
@@ -92,7 +127,7 @@ def save_scrapely_object(spider_name, country_code, username, scrapely_templates
     log.msg('Scrapely Scraper instance is saved at %s' % kipp_merchant_settings_dir)
 
 
-def save_kipp_config(spider_name, country_code, username, merchant_settings):
+def save_kipp_config(spider_name, country_code, merchant_settings):
     """
     Saving spider configuration file for kipp on disk
     :param spider_name:
@@ -101,8 +136,7 @@ def save_kipp_config(spider_name, country_code, username, merchant_settings):
     :param merchant_settings:
     :return:
     """
-    kipp_merchant_settings_dir = KIPP_MERCHANT_SETTINGS_DIR.format(username=username,country_code=country_code,
-                                                                 spider_name=spider_name)
+    kipp_merchant_settings_dir = KIPP_MERCHANT_SETTINGS_DIR.format(country_code=country_code, spider_name=spider_name)
     if not os.path.exists(kipp_merchant_settings_dir):
         os.makedirs(kipp_merchant_settings_dir)
     merchant_file_name = "%s.py" % spider_name
@@ -211,14 +245,16 @@ def publish_kipp_settings(username, country_code, spider_name):
                                                                  country_code=country_code,
                                                                  spider_name=spider_name)
     kipp_config_file_path = kipp_country_setting_dir + '/%s.py' % spider_name
-    scrapely_config_file_path = kipp_country_setting_dir + '/%s.json' % spider_name
-    if os.path.exists(kipp_config_file_path) and os.path.exists(scrapely_config_file_path):
+    portia_config_file_path = kipp_country_setting_dir + '/%s.json' % spider_name
+    temlate_dir = os.path.join(kipp_country_setting_dir, "templates")
+    if os.path.exists(kipp_config_file_path) and os.path.exists(portia_config_file_path):
         #TODO: try and catch exceptions
         repo = Repo(KIPP_SETTINGS_REPO)
         config = repo.config_writer()
         config.set_value("user", "name", username)
         index = repo.index
-        index.add([kipp_config_file_path, scrapely_config_file_path])
+        index.add([kipp_config_file_path, portia_config_file_path])
+        index.add([temlate_dir])
         commit_msg = '%s: Update %s[%s]spider configurations' % (username, spider_name, country_code)
         index.commit(commit_msg)
         origins = repo.remotes
