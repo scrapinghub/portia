c6415ad20c191f13014f564c2398de577553b4e2
Ruairí Ó Fathaigh <ruairi.o.fathaigh@outlook.com>
2019-06-18 18:56:07 +0100

    Update Django and requests library versions

--- portia_server/portia_server/urls.py
+++ portia_server/portia_server/urls.py
@@ -4,6 +4,6 @@ from . import views
 from portia_api import urls
 
 urlpatterns = [
-    url(r'^api/', include(urls, namespace='api')),
+    url(r'^api/', include((urls, 'api'), namespace='api')),
     url(r'^server_capabilities$', views.capabilities),
 ]
