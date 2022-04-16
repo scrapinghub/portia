e8539e1a8eba7e20226797fd61cef9c2813c2345
Milan Hauth <milahu@gmail.com>
2022-04-16 21:47:34 +0200

    portiaui: prettier log messages for websocket

--- portiaui/app/services/web-socket.js
+++ portiaui/app/services/web-socket.js
@@ -70,7 +70,7 @@ export default Service.extend(Evented, {
         this.set('closed', true);
         this.set('connecting', false);
 
-        Logger.log('<Closed Websocket>');
+        Logger.log('Websocket close');
         if(e.code !== APPLICATION_UNLOADING_CODE && e.code !== 1000) {
             if (!window.navigator.onLine) {
                 this.set('reconnectMessage',
@@ -116,7 +116,7 @@ export default Service.extend(Evented, {
     },
 
     _onopen() {
-        Logger.log('<Opened Websocket>');
+        Logger.log('Websocket open');
         this.set('closed', false);
         this.set('reconnectMessage', '');
         this.set('connecting', false);
